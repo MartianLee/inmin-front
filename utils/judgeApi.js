0b35814f489cccdead680c6b7e1708eaaaed6a30
MartianLee <sounghwa777@naver.com>
2017-09-02 03:28:06 +0900

    api url update

--- utils/judgeApi.js
+++ utils/judgeApi.js
@@ -1,30 +1,32 @@
 import axios from 'axios'
 
+const apiUrl = 'http://slb-453338.ncloudslb.com:8080/'
+
 export function getJustices () {
-  return axios.get('http://211.249.62.6:8080/justices')
+  return axios.get(apiUrl + 'justices')
 }
 
 export function getJustice (index) {
-  return axios.get('http://211.249.62.6:8080/justices/' + index)
+  return axios.get(apiUrl + 'justices/' + index)
 }
 
 export function postJustice (judgement) {
-  return axios.post('http://211.249.62.6:8080/justices/judgements', judgement)
+  return axios.post(apiUrl + 'justices/judgements', judgement)
 }
 
 export function getJudgements (justiceId) {
-  return axios.get('http://211.249.62.6:8080/justices/judgements', {params: {'justiceId': justiceId}})
+  return axios.get(apiUrl + 'justices/judgements', {params: {'justiceId': justiceId}})
 }
 
 export function getComments (justiceId) {
-  return axios.get('http://211.249.62.6:8080/justices/comments', {params: {'justiceId': justiceId}})
+  return axios.get(apiUrl + 'justices/comments', {params: {'justiceId': justiceId}})
 }
 
 export function postComment (comment) {
-  return axios.post('http://211.249.62.6:8080/justices/comments', comment)
+  return axios.post(apiUrl + 'justices/comments', comment)
 }
 
 export function getStatistics (justiceId) {
-  return axios.get('http://211.249.62.6:8080/justices/statistic/' + justiceId)
+  return axios.get(apiUrl + 'justices/statistic/' + justiceId)
 }
 
