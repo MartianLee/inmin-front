7a6b0e7de9b4a1bbf0ded8b7f79cee138fa3c6cf
MartianLee <sounghwa777@naver.com>
2017-09-02 03:43:49 +0900

    project content update

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'TAGGERS PRODUCT' }
+      { hid: 'description', name: 'description', content: '국민 여러분이 직접 재판하세요!' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
